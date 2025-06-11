1ef64bdb588d553c4b59a2af721e9f14d49cb5a2
Sergio Duran <538ser@gmail.com>
2025-06-11 13:06:06 -0600

    Chnages in the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,22 +2,15 @@
 export default function NamePage() {
     return (
         <div className="relative min-h-screen flex items-center justify-center overflow-hidden">
-
-            {/* Background image */}
-            <div className="absolute inset-0 z-0 right-0">
-                <img src="header-bg.jpg" className="w-full h-full object-cover" alt="" />
-                <div className="absolute inset-0 bg-black bg-opacity-60"></div>
-            </div>
-
             {/* Content */}
-            <div className="container px-4 md:px-6 relative z-10 space-y-6">
+            <div className="container px-4 md:px-6 relative z-10 space-y-6 text-center max-w-7xl">
+                <p className="uppercase text-metallic-ironGray">Somos Taller de Soldadura Rodriguez</p>
+                <h1 className="tracking-tighter dark:text-white">Confianza y calidad <span className="text-metallic-patina dark:text-cyan-400 font-black">en cada punto de soldadura</span></h1>
+                <p className="text-2xl text-slate-500">Solucionamos problemas críticos de soldadura con servicios hechos a medida: limpieza láser, reparación de piezas y <br /> fabricación de componentes personalizados
+                </p>
 
-                <h1 className="font-black">Soldadura de Precision y <br /> <span className="text-cyan-300 font-black tracking-tight"> Limpieza Laser</span></h1>
-                <p>Expertos en soldadura industrial, muflas, portones, gondolas para vagonetas, plataformas y mas...</p>
-
-                <div className="flex items-center gap-4">
-                    <button className="outline outline-1 outline-cyan-400 px-4 py-3 rounded-md font-bold hover:bg-cyan-900 transition-all duration-300">Explora nuestros servicios</button>
-                    <button className="outline outline-1 outline-white px-4 py-3 rounded-md font-bold hover:bg-cyan-500 transition-all duration-300">Contactanos</button>
+                <div className="flex items-center justify-center">
+                    <button className="bg-metallic-patina px-6 py-3 rounded-md font-bold hover:bg-cyan-900 transition-all duration-300 text-white">Comunicate con nosotros</button>
                 </div>
             </div>
         </div>
